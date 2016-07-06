17cc1ad6f3a468fc67b6159e216a19dcf2d5c85c
Nata <Nata@NATALI>
2016-07-06 22:23:12 +0300

    Partial realization of the third task

---
+++ ClassLibraryBitsOperation/BitsOperation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryBitsOperation
+{
+    public class BitsOperation
+    {
+        #region Method InsertNumber
+        /// <summary>
+        ///Method InsertNumber insert sNumber to fNumber from position iPos to jPos
+        /// </summary>
+        /// <param name="fNumber">The Number in wich insert</param>
+        /// <param name="sNumber">Insert number</param>
+        /// <param name="iPos">First position to insert</param>
+        /// <param name="jPos">Last position to insert</param>
+        /// <returns>Combine number</returns>
+        public static int InsertNumber(int fNumber, int sNumber, int iPos, int jPos)
+        {
+            int fTotal = GetNumberOfBits(fNumber);
+            int sTotal = GetNumberOfBits(sNumber);
+
+            int countBits = jPos - iPos;
+
+            if (iPos > jPos || iPos == jPos)
+                throw new ArgumentException();
+
+            if (iPos < 0 || jPos < 0 || iPos > 30 || jPos > 30)
+                throw new ArgumentException();
+
+            if (sTotal > countBits)
+                throw new ArgumentException();
+
+            if (fTotal < iPos || sTotal <= countBits)
+            {
+                for (int i = iPos - 1, j = 0; i < jPos; i++, j++)
+                {
+                    if (((sNumber >> j) & 1) == 1)
+                        fNumber |= 1 << i;
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+
+            return fNumber;
+        }
+        #endregion
+
+        #region Helper Methods
+        private static int GetNumberOfBits(int number)
+        {
+            int order = 0;
+            while (number > 1)
+            {
+                number /= 2;
+                order++;
+            }
+            return order;
+        }
+        #endregion
+    }
+}
---
+++ ClassLibraryBitsOperationNunit/BitsOpeartionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryBitsOperation;
+using NUnit.Framework;
+
+namespace ClassLibraryBitsOperationNunit
+{
+    [TestFixture]
+    public class BitsOperationTests
+    {
+        [TestCase(7, 4, 18, 15)]
+        [TestCase(7, 4, 37, 64)]
+        [TestCase(7, 4, 7, 8)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InsertNumberMethodTest_Insert4In7_ReturnedException(int fNumber, int sNumber, int iPos, int jPos)
+        {
+            BitsOperation.InsertNumber(fNumber, sNumber, iPos, jPos);
+        }
+
+        [TestCase(7, 4, 8, 15)]
+        public void InsertNumberMethodTest_Insert4In7_Returned519(int fNumber, int sNumber, int iPos, int jPos)
+        {
+            int expected = 519;
+
+            int actual = BitsOperation.InsertNumber(fNumber, sNumber, iPos, jPos);
+
+            Assert.AreEqual(expected, actual, "{0} != {1}", expected, actual);
+        }
+    }
+}
